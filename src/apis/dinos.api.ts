4df9935001cd7e0ada57d8b81f75f993bedca985
Pontus Doverstav <doverstav@gmail.com>
2021-11-30 23:39:11 +0100

    Remove body consumption

--- src/apis/dinos.api.ts
+++ src/apis/dinos.api.ts
@@ -2,7 +2,6 @@ export const fetchDino = async (): Promise<Response> => {
   try {
     console.log('Fetching dino')
     const response = await fetch('/api/dinos')
-    console.log(await response.json())
     return response
   } catch (error) {
     console.error(error)
