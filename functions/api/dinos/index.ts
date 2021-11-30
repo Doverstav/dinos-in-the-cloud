b0c71cc0414af324f0214ca826d0d973888c1eff
Pontus Doverstav <doverstav@gmail.com>
2021-11-30 23:15:04 +0100

    Fix dumb bug :(

--- functions/api/dinos/index.ts
+++ functions/api/dinos/index.ts
@@ -1,9 +1,8 @@
 export const onRequestGet = () => {
   const dinos = ["Velociraptor", "Stegosaurus", "Tyrannosaurus Rex", "Triceratops"]
   const response = {
-    dino: dinos[Math.random() * dinos.length]
+    dino: dinos[Math.round(Math.random() * dinos.length)]
   }
 
-  //return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
-  return new Response(JSON.stringify({dino: "T-Rex"}), { headers: { "Content-Type": "application/json" } })
+  return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
 }
