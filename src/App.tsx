21db1db821101fab85e21fdc73f99d07de2daf2a
Pontus Doverstav <doverstav@gmail.com>
2021-11-30 23:32:41 +0100

    Add try catch

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import dino from './dino.svg';
-import './App.css';
-import { fetchDino } from './apis/dinos.api';
+import React, { useEffect, useState } from "react";
+import dino from "./dino.svg";
+import "./App.css";
+import { fetchDino } from "./apis/dinos.api";
 
 function App() {
-  const [randomDino, setDino] = useState<string>()
+  const [randomDino, setDino] = useState<string>();
 
   useEffect(() => {
     const dinoFetcher = async () => {
-      const response = await fetchDino()
-      const json = await response.json()
-      console.log(json)
-      setDino(json.dino)
-    }
-    dinoFetcher()
-  }, [])
+      try {
+        const response = await fetchDino();
+        const json = await response.json();
+        console.log(json);
+        setDino(json.dino);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    dinoFetcher();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={dino} className="App-logo" alt="logo" />
-        <p>
-          The dinosaur you get is: {randomDino}
-        </p>
+        <p>The dinosaur you get is: {randomDino}</p>
       </header>
     </div>
   );
