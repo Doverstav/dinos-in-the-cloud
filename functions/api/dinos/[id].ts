c4901bebdb9d9523077941dc95ace65b5cf37f08
Pontus Doverstav <doverstav@gmail.com>
2021-12-05 21:47:35 +0100

    Can I add to KV?

--- functions/api/dinos/[id].ts
+++ functions/api/dinos/[id].ts
@@ -1,13 +1,29 @@
+import { env } from "process"
+
+interface UserData {
+  dinosLiked: string[]
+  dinosLikedAmount: number
+}
+
+interface RequestBody {
+  dino: string
+}
+
 export const onRequestPost = async (context) => {
   const {
     params,
     request
   }: { request: Request, params: string } = context
 
-  const response = {
-    id: params,
-    body: await request.json()
+  const body = (await request.json()) as RequestBody
+  const currentUserData = (await env.DINOS.get(params, { type: "json" })) as UserData
+
+  const newUserData = {
+    dinosLiked: currentUserData.dinosLiked.push(body.dino),
+    dinosLikedAmount: currentUserData.dinosLikedAmount + 1
   }
 
-  return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
+  await env.DINOS.put(params, newUserData)
+
+  return new Response(JSON.stringify(newUserData), { headers: { "Content-Type": "application/json" } })
 }
