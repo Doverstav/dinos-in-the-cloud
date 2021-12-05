7558a84db95443dfcba32644dddb40097e9d2086
Pontus Doverstav <doverstav@gmail.com>
2021-12-05 21:02:43 +0100

    Try to read body correctly

--- functions/api/dinos/[id].ts
+++ functions/api/dinos/[id].ts
@@ -1,12 +1,12 @@
-export const onRequestPost = (context) => {
+export const onRequestPost = async (context) => {
     const {
         params,
         request
-    } = context
+    }: { request: Request, params: string } = context
 
     const response = {
         id: params,
-        body: request.body
+        body: await request.json()
     }
 
     return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
