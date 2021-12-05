e2794aed2a19dab9ba18474f98874e5d75fc279f
Pontus Doverstav <doverstav@gmail.com>
2021-12-05 21:37:18 +0100

    Format file

--- functions/api/dinos/[id].ts
+++ functions/api/dinos/[id].ts
@@ -1,13 +1,13 @@
 export const onRequestPost = async (context) => {
-    const {
-        params,
-        request
-    }: { request: Request, params: string } = context
+  const {
+    params,
+    request
+  }: { request: Request, params: string } = context
 
-    const response = {
-        id: params,
-        body: await request.json()
-    }
+  const response = {
+    id: params,
+    body: await request.json()
+  }
 
-    return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
+  return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } })
 }
